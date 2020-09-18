ac551c20446085bcca37f965c38da49c5817aa69
Prajwal rao p <71481375+99002495@users.noreply.github.com>
2020-09-18 17:45:43 +0530

    Update calculator.c

--- src/calculator.c
+++ src/calculator.c
@@ -1,19 +1,55 @@
 #include "calculator.h"
+#include<math.h>
 
-int prime_number(int number)
+/* input 2 number with choice of opperator*/
+/*this programme uses function Pointer array */
+int calci(int num1,int num2,int choice)
 {
-    /**
-    *@note Prime numbers can't be divided by other numbers than itself or 1
-    *@note for loop is used to check the reminder of given number is 0 when divided with 2
-    */
-    for(int i=2;i<=number/2;i++)
-    {
 
-       //checks whether the reminder is 0 or not
-        //if 0, the given number is not a prime number
 
-        if(number%i == 0)
-           return 0;
-    }
-    return 1;
+
+int add(int num1, int num2);
+int subtract(int num1, int num2);
+int multiply(int num1, int num2);
+int divide(int num1, int num2);
+
+
+
+  int  result;
+  int (*op[4])(int, int);
+
+  op[0] = add;//choice 1 for add
+  op[1] = subtract;//choice 2 for sub
+  op[2] = multiply;//choice 3 for multiplication
+  op[3] = divide;// choice 4 for divide
+
+
+
+  result = op[choice](num1, num2);
+
+
+  return result;
+}
+/*$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$*/
+// functions to add, mull, sub, divide
+
+int add(int num1, int num2) {
+  return(num1 + num2);
+}
+
+int subtract(int num1, int num2) {
+  return(num1 - num2);
+}
+
+int multiply(int num1, int num2) {
+  return(num1 * num2);
+}
+
+int divide(int num1, int num2) {
+  if (num2 != 0)
+    return (num1 / num2);
+  else
+    return 0;
 }
